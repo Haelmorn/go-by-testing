2fd6f88f9d17c67dd6f44dddd0e4c478f9de726f
Haelmorn <haelmorn@gmail.com>
2020-12-22 02:46:00 +0100

    Refactored arrnsl, added benchmarks and examples

--- arrnsl/arrnsl.go
+++ arrnsl/arrnsl.go
@@ -1,5 +1,6 @@
 package arrnsl
 
+//Returns a sum of all ints in slice
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
@@ -7,4 +8,29 @@ func Sum(numbers []int) int {
 	}
 
 	return sum
+}
+
+//Returns a slice, where each index is a Sum of slice passed
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, nums := range numbersToSum {
+		if len(nums) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := nums[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
 }
--- arrnsl/arrnsl_test.go
+++ arrnsl/arrnsl_test.go
@@ -1,6 +1,10 @@
 package arrnsl
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -24,4 +28,74 @@ func TestSum(t *testing.T) {
 			t.Errorf("Got %d want %d, given %d", got, want, numbers)
 		}
 	})
+}
+
+func BenchmarkSum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sum([]int{1, 2, 3, 4, 5})
+	}
+}
+
+func ExampleSum() {
+	sum := Sum([]int{1, 2, 3})
+	fmt.Println(sum)
+	// output: 6
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got %d want %d", got, want)
+	}
+
+}
+
+func BenchmarkSumAll(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAll([]int{1, 2, 3, 4, 5}, []int{4, 5, 6})
+	}
+}
+
+func ExampleSumAll() {
+	sum := SumAll([]int{1, 2, 3}, []int{3, 4, 5}, []int{5, 6, 7})
+	fmt.Println(sum)
+	// output: [6 12 18]
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %d want %d", got, want)
+		}
+	}
+	t.Run("a sum of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("sum of empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{1, 2, 3})
+		want := []int{0, 5}
+
+		checkSums(t, got, want)
+	})
+}
+
+func BenchmarkSumAllTails(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAll([]int{1, 2, 3, 4, 5}, []int{4, 5, 6})
+	}
+}
+
+func ExampleSumAllTails() {
+	sum := SumAllTails([]int{1, 2, 3}, []int{3, 4, 5}, []int{5, 6, 7})
+	fmt.Println(sum)
+	// output: [5 9 13]
 }
